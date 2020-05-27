6e400dfa428740d3008312bde46d7c63b17eefb9
dibyajyoti47 <dibyajyoti.6025@gmail.com>
2020-05-27 23:18:59 +0530

    initial commit lession 23 compl

---
+++ core/src/main/java/academy/learning/Main.java
@@ -0,0 +1,12 @@
+package academy.learning;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+	private static final Logger log = LoggerFactory.getLogger(Main.class);
+	public static void main(String[] args) {
+		log.info("this is info logger");
+		log.debug("this is debugc logger");
+	}
+}
