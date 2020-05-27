28b42a5fcc343ffd5c133cd0cad192acd7b75cc4
dibyajyoti47 <dibyajyoti.6025@gmail.com>
2020-05-27 23:27:24 +0530

    updated folder

---
+++ hello_maven/src/main/java/learn/dj/HelloMaven.java
@@ -0,0 +1,14 @@
+package learn.dj;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloMaven {
+	private final static Logger log = LoggerFactory.getLogger(HelloMaven.class);
+	public static void main(String[] args) {
+		log.info("hello INFO");
+		log.debug("hello Debug");
+	}
+
+}
